8684652901fab66372a3cf65bec5b457ad0dd516
Serg Chernata <sergchernata@gmail.com>
2021-10-02 09:43:59 -0400

    format integer values

--- app/helpers/from-lamport.js
+++ app/helpers/from-lamport.js
@@ -1,8 +1,9 @@
 import { helper } from '@ember/component/helper';
 
 export function from_lamport([value]) {
-  const result = (value / 1000000000).toFixed(9);
-  return result.slice(-1) === '0' ? parseFloat(result) : result;
+  let result = (value / 1000000000).toFixed(9);
+  result = result.slice(0, 1) === '0' ? parseFloat(result) : parseInt(result, 10).toLocaleString();
+  return result;
 }
 
 export default helper(from_lamport);
