2ff0689e1a12a3daad2f980e104088e33880f1f5
Serg Chernata <sergchernata@gmail.com>
2023-04-08 11:45:37 -0400

    upgraded ember and ember-cli

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
   printWidth: 140,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
 };
